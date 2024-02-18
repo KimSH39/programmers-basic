8745681599d50f847b682b8c12e156d2fb831a20
Sohyeon Kim <sooooooohyeon5@naver.com>
2024-02-18 14:17:17 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,13 @@
+def solution(citations):
+    
+    answer = 0
+    
+    citations.sort(reverse = True)
+    
+    for i in range(len(citations)):
+        if citations[i] > answer:
+            answer += 1
+        else:
+            break
+    
+    return answer
